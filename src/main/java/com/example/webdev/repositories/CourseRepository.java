a41f81caeb0126edd73c1ceb21e5f12bbb955685
amyyuqian <amyyuqian@gmail.com>
2018-05-19 14:50:40 -0400

    add course repository and service

---
+++ src/main/java/com/example/webdev/repositories/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.example.webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdev.models.Course;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> { 
+	
+}
+
