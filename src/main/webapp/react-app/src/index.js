af11b1da8420d288ca238e14d4065465cd7377a6
amyyuqian <amyyuqian@gmail.com>
2018-05-19 15:10:40 -0400

    move CourseManager.js to containers folder

--- src/main/webapp/react-app/src/index.js
+++ src/main/webapp/react-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CourseManager from './containers/CourseManager';
 
 ReactDOM.render(
- <h1>Hello, world!</h1>,
- document.getElementById('root')
+   <CourseManager/>,
+   document.getElementById('root')
 );
+
